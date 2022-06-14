2d061b8fd9b5bedff8dcd9efe24d97aef67a60ad
B.V. Avinash <78400699+BVAvinash@users.noreply.github.com>
2022-06-14 15:32:10 +0530

    Delete print_ColorCodeManual.cpp

--- print_ColorCodeManual.cpp
+++
@@ -1,7 +0,0 @@
-#include "run_TelCoColorCoder.hpp"
-
-void printManual(int pairNumber) 
-{
-	TelCoColorCoder::ColorPair colorPair = TelCoColorCoder::GetColorFromPairNumber(pairNumber);
-	std::cout << pairNumber << " \t " << colorPair.ToString() << std::endl;
-}
